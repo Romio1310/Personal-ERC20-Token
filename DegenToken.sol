8c8b30710b9e8d18a7e504e54f6ed942b8687cee
Gurdeep Singh <137512027+Romio1310@users.noreply.github.com>
2024-08-19 18:15:44 +0530

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
@@ -9,23 +8,50 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable{
 
-    constructor() ERC20("Degen","DGN") Ownable(msg.sender){}
+    mapping(string => uint256) public items;      // Stores item names with their corresponding token cost
+    string[] public itemNames;                    // Tracks item names
 
-    function mint(address to, uint256 amount) external onlyOwner{
-        _mint(to, amount);
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        items["Sword"] = 1 * 10 ** decimals();    // Sword costs 1 DegenToken
+        items["Shield"] = 2 * 10 ** decimals();   // Shield costs 2 DegenTokens
+        items["Potion"] = 5 * 10 ** decimals();   // Potion costs 5 DegenTokens
+
+        itemNames.push("Sword");                  // Add Sword to item list
+        itemNames.push("Shield");                 // Add Shield to item list
+        itemNames.push("Potion");                 // Add Potion to item list
     }
 
-    function transferToken(address _receiver, uint256 _value) external{
-        require(balanceOf(msg.sender) >= _value,"Insufficient balance");
-        _transfer(msg.sender, _receiver, _value);
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);                        // Mint tokens to a specified address
     }
 
-    function burnToken(uint256 _value) external{
-        require(balanceOf(msg.sender) >= _value,"Insufficient balance");
-        _burn(msg.sender, _value);
+    function transferToken(address receiver, uint256 value) external {
+        require(balanceOf(msg.sender) >= value, "Insufficient balance");
+        _transfer(msg.sender, receiver, value);   // Transfer tokens from sender to receiver
     }
 
-    function checkBalance(address account) external view returns (uint256){
-        return balanceOf(account);
+    function burnToken(uint256 value) external {
+        require(balanceOf(msg.sender) >= value, "Insufficient balance");
+        _burn(msg.sender, value);                 // Burn tokens from sender's balance
+    }
+
+    function checkBalance(address account) external view returns (uint256) {
+        return balanceOf(account);                // Return the balance of a specified account
+    }
+
+    function redeem(string memory itemName) external {
+        uint256 itemCost = items[itemName];
+        require(itemCost > 0, "Item does not exist");
+        require(balanceOf(msg.sender) >= itemCost, "Insufficient balance to redeem item");
+        _burn(msg.sender, itemCost);              // Burn tokens to redeem the specified item
+    }
+
+    function addItem(string memory itemName, uint256 tokenCost) external onlyOwner {
+        items[itemName] = tokenCost;              // Add or update an item with its token cost
+        itemNames.push(itemName);                 // Add the item name to the list
+    }
+
+    function getAllItems() external view returns (string[] memory) {
+        return itemNames;                         // Return the list of all available item names
     }
 }
